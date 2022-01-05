6ac9237469a4f8bd1aa8ad2deaa444bd10612658
andy <admazzola@gmail.com>
2022-01-05 01:39:18 -0500

    eth symbol

--- src/views/RecentActivity.vue
+++ src/views/RecentActivity.vue
@@ -268,7 +268,7 @@ export default {
               let decimals = 18 
               let currencyAmountFormatted = MathHelper.rawAmountToFormatted(result.currencyTokenAmount,decimals)
                 currencyAmountFormatted = MathHelper.formatFloat(currencyAmountFormatted)
-                currencyAmountFormatted = currencyAmountFormatted.toString().concat(' ♦')
+                currencyAmountFormatted = currencyAmountFormatted.toString().concat(' Ξ')
 
                let iconURL = AssetDataHelper.getImageURL(collectionName,result.nftTokenId) 
 
